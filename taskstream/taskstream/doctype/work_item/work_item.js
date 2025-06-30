5599ff767d360f6b6a0cc4edcfacce8f26476c60
chethank1407 <chethank1407@gmail.com>
2025-06-30 11:51:55 +0530

    feat: added creator fetch

--- taskstream/taskstream/doctype/work_item/work_item.js
+++ taskstream/taskstream/doctype/work_item/work_item.js
@@ -1,8 +1,29 @@
 // Copyright (c) 2025, Chethan - Aerele and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Work Item", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on('Work Item', {
+	onload: function (frm) {
+		if (frm.is_new() && !frm.doc.requested_by) {
+			frappe.call({
+				method: 'frappe.client.get_list',
+				args: {
+					doctype: 'Employee',
+					filters: {
+						user_id: frappe.session.user
+					},
+					fields: ['name']
+				},
+				callback: function (r) {
+					if (r.message && r.message.length > 0) {
+						frm.set_value('requested_by', r.message[0].name);
+					}
+				}
+			});
+		}
+	},
+	refresh: function (frm) {
+		if (frm.is_new() && !frm.doc.requested_by) {
+			frm.trigger('onload');
+		}
+	}
+});
