0f7e5460d40ddfb927171588d8e73c30e23f5674
chethank1407 <chethank1407@gmail.com>
2025-07-02 22:15:20 +0530

    update: updated requested by

--- taskstream/taskstream/doctype/work_item/work_item.js
+++ taskstream/taskstream/doctype/work_item/work_item.js
@@ -4,21 +4,7 @@
 frappe.ui.form.on('Work Item', {
 	onload: function (frm) {
 		if (frm.is_new() && !frm.doc.requested_by) {
-			frappe.call({
-				method: 'frappe.client.get_list',
-				args: {
-					doctype: 'Employee',
-					filters: {
-						user_id: frappe.session.user
-					},
-					fields: ['name']
-				},
-				callback: function (r) {
-					if (r.message && r.message.length > 0) {
-						frm.set_value('requested_by', r.message[0].name);
-					}
-				}
-			});
+			frm.set_value('requested_by', frappe.session.user);
 		}
 		if (frm.is_new() && !frm.doc.planned_start) {
 			frm.set_value('planned_start', frappe.datetime.now_datetime());
