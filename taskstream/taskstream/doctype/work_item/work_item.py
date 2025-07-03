d7ddc7aec6cd9b9e8fb66a71f3e85ca2e4574214
chethank1407 <chethank1407@gmail.com>
2025-07-03 17:19:02 +0530

    feat: added maker checker validation

--- taskstream/taskstream/doctype/work_item/work_item.py
+++ taskstream/taskstream/doctype/work_item/work_item.py
@@ -13,3 +13,27 @@ class WorkItem(Document):
 			old_duration = frappe.db.get_value("Work Item", self.name, "estimated_duration")
 			if old_duration != self.estimated_duration:
 				self.revision_count = self.revision_count + 1
+
+@frappe.whitelist()
+def send_for_review(docname):
+	doc = frappe.get_doc("Work Item", docname)
+	
+	reviewer_email = frappe.db.get_value("User", doc.reviewer, "email")
+	if not reviewer_email:
+		frappe.throw("Reviewer does not have a valid email.")
+
+	frappe.sendmail(
+		recipients=[reviewer_email],
+		subject=f"Review Requested for Work Item: {doc.name}",
+		message=f"A critical work item has been submitted for your review.<br><br><b>Name:</b> {doc.name}<br><b>Assignee:</b> {doc.assignee}<br><b>Started on:</b> {doc.actual_start}<br><br><a href='{frappe.utils.get_url()}/app/work-item/{doc.name}'>View Work Item</a>"
+	)
+
+	doc.status = "Under Review"
+	doc.save(ignore_permissions=True)
+
+@frappe.whitelist()
+def mark_complete(docname):
+	doc = frappe.get_doc("Work Item", docname)
+
+	doc.status = "Done"
+	doc.save(ignore_permissions=True)
