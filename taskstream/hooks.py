81c7df6ee4526d41aa64f1448192fc22619c85ed
Afsal Syed <afsalsyed12@gmail.com>
2026-04-01 10:45:49 +0530

    feat: Enable Custom UI

--- taskstream/hooks.py
+++ taskstream/hooks.py
@@ -25,7 +25,7 @@ app_license = "mit"
 # ------------------
 
 # include js, css files in header of desk.html
-# app_include_css = ["taskstream.bundle.css"]
+app_include_css = ["taskstream.bundle.css"]
 # app_include_js = "/assets/taskstream/js/taskstream.js"
 
 # include js, css files in header of web template
