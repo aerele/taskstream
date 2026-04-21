1b5db5e7210330f8ac734293d4fc44402c31396d
Afsal Syed <afsalsyed12@gmail.com>
2026-04-21 23:59:29 +0530

    feat: remove date filters from Work Item Score Board report

--- taskstream/taskstream/report/work_item_score_board/work_item_score_board.js
+++ taskstream/taskstream/report/work_item_score_board/work_item_score_board.js
@@ -4,18 +4,6 @@
 frappe.query_reports["Work Item Score Board"] = {
 	filters: [
 		{
-			fieldname: "from_date",
-			label: __("From Date"),
-			fieldtype: "Date",
-			default: frappe.datetime.add_months(frappe.datetime.get_today(), -1),
-		},
-		{
-			fieldname: "to_date",
-			label: __("To Date"),
-			fieldtype: "Date",
-			default: frappe.datetime.get_today(),
-		},
-		{
 			fieldname: "user",
 			label: __("User"),
 			fieldtype: "Link",
