f894a4773c3b55e620d7a5cbbe674e502c0b01f0
Afsal Syed <afsalsyed12@gmail.com>
2026-04-24 16:41:44 +0530

    feat: set default value for req_target_date_time in Work Item form and add validation for time extension request

--- taskstream/taskstream/doctype/work_item_time_extension/work_item_time_extension.py
+++ taskstream/taskstream/doctype/work_item_time_extension/work_item_time_extension.py
@@ -1,12 +1,20 @@
 # Copyright (c) 2026, Chethan - Aerele and contributors
 # For license information, please see license.txt
 
+from datetime import datetime
+
 import frappe
 from frappe.model.document import Document
 from frappe.utils import now_datetime
 
 
 class WorkItemTimeExtension(Document):
+	def validate(self):
+		if isinstance(self.requested_due_date, str):
+			requested_due_date = datetime.strptime(self.requested_due_date, "%Y-%m-%d %H:%M:%S")
+		if self.current_target_date > requested_due_date:
+			frappe.throw("Enter a valid Date")
+
 	def after_insert(self):
 		if self.approver and any(row.user == self.requester for row in self.approver):
 			update_status(self.work_item_reference, "Approved", self)
